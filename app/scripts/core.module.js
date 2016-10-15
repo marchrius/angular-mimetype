c9d7d270485203bd72de2b5f8df151a09a10447d
Matteo Gaggiano <maxxxx92@gmail.com>
2016-10-15 19:03:36 +0200

    Initial commit

---
+++ app/scripts/core.module.js
@@ -0,0 +1,16 @@
+(function() {
+	'use strict';
+
+  /**
+   * @ngdoc overview
+   * @name mg.mimeType
+   * @description
+   * # angular-mimetype
+   *
+   * Main module of the application.
+   */
+  angular
+    .module('mg.mimetype', ['mg.mimeType.filters', 'mg.mimeType.directives']);
+
+
+})();
---
+++ app/scripts/filters/mimetype.filter.js
@@ -0,0 +1,22 @@
+ (function () {
+  'use strict';
+
+  /**
+   * @ngdoc function
+   * @name mg.mimetype.filters.filter:mimeType
+   * @description
+   * # mimeTypeFilter
+   * Filter of the mg.mimetype
+   */
+  angular.module('mg.mimetype.filters')
+    .filter('mimetype', MimeTypeFilter);
+
+  function MimeTypeFilter($log, fileTypeConstants) {
+
+    return mimeTypeFilter() {
+      var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments));
+
+      
+    }
+  };
+})();
