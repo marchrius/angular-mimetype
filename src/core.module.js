e608f876316f67376555fe42538774d4d0ecf9bf
Matteo Gaggiano <maxxxx92@gmail.com>
2016-10-16 18:36:25 +0200

    gh-pages first init

--- src/core.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('mg.mimetype', ['mg.mimetype.filters', 'mg.mimetype.directives', 'mg.mimetype.constants']);
-
-})();
--- src/directives/directives.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('mg.mimetype.directives', []);
-    
-})();
