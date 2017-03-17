2dc54cdccb9e1ad511c989c73c1a0cb559be9ce5
Matteo Gaggiano <maxxxx92@gmail.com>
2017-03-17 10:35:48 +0100

    moved filter code to factory

--- src/filters/mimetype.filter.js
+++ src/filters/mimetype.filter.js
@@ -2,59 +2,11 @@
   'use strict';
 
   angular.module('mg.mimetype.filters')
-    .filter('mimetype', function($util, fileType) {
+    .filter('mimetype', mimetypeFilter);
 
-      var MAX_LEN = 255;
-      var EMPTY_STRING = "".toString();
-
-      return function mimeTypeFilter() {
-        var args = (arguments.length === 1 ? [arguments[0]] : Array.apply(null, arguments));
-        var input = args[0];
-
-        if (input instanceof ArrayBuffer) {
-          return fromArrayBuffer(input);
-        } else if (input instanceof Blob) {
-          return fromBlob(input);
-        } else if (input instanceof String) {
-          return fromString(input);
-        } else {
-          return fromString(input);
-        }
-      };
-
-      function fromString(input) {
-        if (typeof input === 'undefined')
-          return EMPTY_STRING;
-        var found = null,
-          obj = null,
-          part, hex, res;
-        for (var prop in fileType) {
-          if ($util.hop(fileType, prop) && input.startsWith(prop)) {
-            obj = fileType[prop];
-            if (angular.isArray(obj)) {
-              for (var i = 0; i < obj.length; i++) {
-                part = input.substr(0, MAX_LEN);
-                hex = $util.base64ToHex(part, false).toUpperCase();
-                res = test(obj[i].regex, hex);
-                if (res) {
-                  return obj[i].type;
-                }
-              }
-            } else {
-              part = input.substr(0, MAX_LEN);
-              hex = $util.base64ToHex(part, false).toUpperCase();
-              res = test(obj.regex, hex);
-              if (res) {
-                return obj.type;
-              }
-            }
-          }
-        }
-        return EMPTY_STRING;
-      }
-
-      function test(regStr, compStr) {
-        return (new RegExp(regStr)).test(compStr);
-      }
-    });
+  function mimetypeFilter(mimeType) {
+    return function() {
+      return mimeType.fromAuto.apply(mimeType, arguments);
+    };
+  }
 })();
